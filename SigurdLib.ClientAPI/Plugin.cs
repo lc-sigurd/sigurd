38a80c9262d1fb10150569fc55301069d9664a3a
DontTalkToMeThx <steveowebster8@gmail.com>
2024-01-10 14:10:04 -0500

    Bundle loader helper

---
+++ SigurdLib.ClientAPI/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+
+namespace Sigurd.ClientAPI
+{
+    /// <summary>
+    /// The main Plugin class.
+    /// </summary>
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public sealed class Plugin : BaseUnityPlugin
+    {
+        internal static Plugin Instance { get; private set; }
+
+        internal static ManualLogSource Log;
+
+        internal static Harmony Harmony;
+
+        private void Awake()
+        {
+            Instance = this;
+
+            Log = Logger;
+
+            Harmony = new Harmony($"{MyPluginInfo.PLUGIN_GUID}-{DateTime.Now.Ticks}");
+            Harmony.PatchAll();
+
+            Log.LogInfo($"{MyPluginInfo.PLUGIN_NAME} ({MyPluginInfo.PLUGIN_VERSION}) has awoken.");
+        }
+    }
+}
