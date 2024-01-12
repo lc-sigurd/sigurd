7bb69fe4ce28c45f779d26d5b141ff91b07c232d
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-12 15:21:44 +0000

    add thunderstore project model from thunderstore-cli

---
+++ MSBuildTasks/ThunderstoreCLI/Models/ISerialize.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace ThunderstoreCLI.Models;
+
+// https://github.com/thunderstore-io/thunderstore-cli/blob/10b73c843f2dd1a9ed9c6cb687dbbaa555626052/ThunderstoreCLI/Models/ISerialize.cs
+// thunderstore-cli Copyright (c) 2021 Thunderstore
+public interface ISerialize<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>
+    where T : ISerialize<T>
+{
+    public string Serialize();
+#if NET8_0
+    public static abstract T? Deserialize(string input);
+    public static virtual ValueTask<T?> DeserializeAsync(string input)
+    {
+        return new(T.Deserialize(input));
+    }
+    public static virtual async ValueTask<T?> DeserializeAsync(Stream input)
+    {
+        using StreamReader reader = new(input);
+        return T.Deserialize(await reader.ReadToEndAsync());
+    }
+#endif
+}
