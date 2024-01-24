3aa98acdcb8c02799c9bcea2647f737ac0dbaed5
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-24 23:03:30 +0000

    add simple tests for the raw command splitter

---
+++ SigurdLib.Terminal.Tests/Parsing/RawCommandSplitterTests.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using Sigurd.Terminal.Parsing;
+
+namespace SigurdLib.Terminal.Tests.Terminal.Parsing;
+
+public class RawCommandSplitterTests
+{
+    private void AssertSplitTextEquals(string textToSplit, string[] splitText)
+    {
+        Assert.Equal(splitText, RawCommandSplitter.Split(textToSplit).ToArray());
+    }
+
+    [Fact]
+    public void TestSimpleSplit()
+    {
+        AssertSplitTextEquals("foo bar baz", ["foo", "bar", "baz"]);
+    }
+
+    [Fact]
+    public void TestNoSplit()
+    {
+        AssertSplitTextEquals("foobarbaz", ["foobarbaz"]);
+    }
+}
