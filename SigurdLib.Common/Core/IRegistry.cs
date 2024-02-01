0a14f04cc4f8e390501fa21e24b82246bad7f2f7
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-01 17:08:06 +0000

    add missing members; update signatures where necessary

--- SigurdLib.Common/Core/IRegistry.cs
+++ SigurdLib.Common/Core/IRegistry.cs
@@ -6,23 +6,30 @@ namespace Sigurd.Common.Core;
 
 public interface IRegistrar
 {
-    public static TValue Register<TValue, TRegistry>(IRegistry<TRegistry> registry, ResourceLocation name, TValue value)
-        where TValue : TRegistry
-        where TRegistry : class
+    public static TValue Register<TValue>(IRegistrar<TValue> registry, ResourceLocation name, TValue value)
+        where TValue : class
+        => Register(registry, IResourceKey.Create(registry.Key, name), value);
+
+    public static TValue Register<TValue, TValueBase>(IRegistrar<TValueBase> registry, ResourceKey<TValueBase> key, TValue value)
+        where TValue : class, TValueBase
+        where TValueBase : class
     {
         throw new NotImplementedException();
     }
 
-    public static TValue Register<TValue, TRegistry>(IRegistry<TRegistry> registry, ResourceKey<TRegistry> key, TValue value)
-        where TValue : TRegistry
-        where TRegistry : class
+    public static IHolder.Reference<THeld> RegisterForHolder<THeld>(IRegistrar<THeld> registry, ResourceLocation name, THeld value)
+        where THeld : class
+        => RegisterForHolder(registry, IResourceKey.Create(registry.Key, name), value);
+
+    public static IHolder.Reference<THeld> RegisterForHolder<THeld>(IRegistrar<THeld> registry, ResourceKey<THeld> key, THeld value)
+        where THeld : class
     {
         throw new NotImplementedException();
     }
 
     HashSet<ResourceLocation> NameSet { get; }
 
-    bool ContainsName(ResourceLocation name);
+    bool ContainsName(ResourceLocation? name);
 }
 
 public interface IKeyed<out T>
@@ -30,16 +37,22 @@ public interface IKeyed<out T>
     IResourceKey<T> Key { get; }
 }
 
-public interface IRegistrar<out TValue> : IRegistrar, IKeyed<TValue> where TValue : class
+public interface IRegistrar<out TValue> : IRegistrar, IKeyed<IRegistrar<TValue>> where TValue : class
 {
-
+    TValue? Get(ResourceLocation? name);
 }
 
 public interface IRegistry<TValue> : IRegistrar<TValue> where TValue : class
 {
-    HashSet<KeyValuePair<IResourceKey<TValue>, TValue>> EntrySet { get; }
+    ResourceLocation? GetName(TValue value);
 
     HashSet<IResourceKey<TValue>> KeySet { get; }
 
-    bool ContainsKey(IResourceKey<TValue> key);
+    bool ContainsKey(IResourceKey<TValue>? key);
+
+    TValue? Get(IResourceKey<TValue>? key);
+
+    IResourceKey<TValue>? GetKey(TValue value);
+
+    HashSet<KeyValuePair<IResourceKey<TValue>, TValue>> EntrySet { get; }
 }
