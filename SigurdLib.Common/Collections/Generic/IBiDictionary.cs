f2fe645842cd6234cb292ca58f3d84fedbd4b3d5
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-07 03:46:10 +0000

    update BiDictionary implementation to allow indexing when the key and value types are the same

--- SigurdLib.Common/Collections/Generic/IBiDictionary.cs
+++ SigurdLib.Common/Collections/Generic/IBiDictionary.cs
@@ -11,14 +11,10 @@ namespace Sigurd.Common.Collections.Generic;
 /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
 public interface IBiDictionary<TKey, TValue> : IDictionary<TKey, TValue>
 {
-    /// <summary>Gets or sets the element with the specified value.</summary>
-    /// <param name="valueKey">The value of the element to get or set.</param>
-    /// <returns>The element with the specified value.</returns>
-    /// <exception cref="T:System.ArgumentNullException"><paramref name="valueKey" /> is <see langword="null" />.</exception>
-    /// <exception cref="T:System.Collections.Generic.KeyNotFoundException">The property is retrieved and <paramref name="valueKey" /> is not found.</exception>
-    /// <exception cref="T:System.NotSupportedException">The property is set and the <see cref="IBiDictionary{TKey, TValue}" /> is read-only.</exception>
-    /// <exception cref="T:System.InvalidOperationException">The property is set and the key is already present, bound to a different value.</exception>
-    TKey this[TValue valueKey] { get; set; }
+    /// <summary>
+    /// The corresponding inverse <see cref="IBiDictionary{TKey,TValue}"/> that allows indexing by value.
+    /// </summary>
+    IBiDictionary<TValue, TKey> Inverse { get; }
 
     /// <summary>Determines whether the <see cref="IBiDictionary{TKey, TValue}" /> contains an element with the specified value.</summary>
     /// <param name="value">The value to locate in the <see cref="IBiDictionary{TKey, TValue}" />.</param>
@@ -34,7 +30,7 @@ public interface IBiDictionary<TKey, TValue> : IDictionary<TKey, TValue>
     /// <see langword="true" /> if the element is successfully removed; otherwise, <see langword="false" />.  This method also returns <see langword="false" /> if <paramref name="value" /> was not found in the original <see cref="IBiDictionary{TKey, TValue}" />.</returns>
     /// <exception cref="T:System.ArgumentNullException"><paramref name="value" /> is <see langword="null" />.</exception>
     /// <exception cref="T:System.NotSupportedException">The <see cref="IBiDictionary{TKey, TValue}" /> is read-only.</exception>
-    bool Remove(TValue value);
+    bool RemoveValue(TValue value);
 
     /// <summary>Gets the key associated with the specified value.</summary>
     /// <param name="value">The value whose key to get.</param>
--- SigurdLib.Common/Collections/ObjectModel/BiDictionary.cs
+++ SigurdLib.Common/Collections/ObjectModel/BiDictionary.cs
@@ -14,6 +14,7 @@ public class BiDictionary<TKey, TValue> : IBiDictionary<TKey, TValue>
 {
     private readonly Dictionary<TKey, TValue> _forward;
     private readonly Dictionary<TValue, TKey> _backward;
+    private InverseImpl? _inverse;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="BiDictionary{TKey, TValue}"/> class that contains no elements,
@@ -104,6 +105,10 @@ public class BiDictionary<TKey, TValue> : IBiDictionary<TKey, TValue>
     }
 
     /// <inheritdoc />
+    [CollectionAccess(CollectionAccessType.Read | CollectionAccessType.ModifyExistingContent)]
+    public IBiDictionary<TValue, TKey> Inverse => _inverse ??= new InverseImpl(this);
+
+    /// <inheritdoc />
     [CollectionAccess(CollectionAccessType.Read)]
     public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => _forward.GetEnumerator();
 
@@ -124,7 +129,16 @@ public class BiDictionary<TKey, TValue> : IBiDictionary<TKey, TValue>
 
     /// <inheritdoc />
     [CollectionAccess(CollectionAccessType.Read)]
-    public bool Contains(KeyValuePair<TKey, TValue> item) => _forward.Contains(item);
+    public bool Contains(KeyValuePair<TKey, TValue> item)
+    {
+        if (!_forward.TryGetValue(item.Key, out var value))
+            return false;
+
+        if (ReferenceEquals(value, null))
+            return false;
+
+        return _backward.Comparer.Equals(value, item.Value);
+    }
 
     /// <inheritdoc />
     [CollectionAccess(CollectionAccessType.Read)]
@@ -185,7 +199,7 @@ public class BiDictionary<TKey, TValue> : IBiDictionary<TKey, TValue>
 
     /// <inheritdoc />
     [CollectionAccess(CollectionAccessType.ModifyExistingContent | CollectionAccessType.Read)]
-    public bool Remove(TValue value)
+    public bool RemoveValue(TValue value)
     {
         var valueWasFound = _backward.Remove(value, out var key);
         if (!valueWasFound) return false;
@@ -202,7 +216,7 @@ public class BiDictionary<TKey, TValue> : IBiDictionary<TKey, TValue>
 
     /// <inheritdoc />
     [CollectionAccess(CollectionAccessType.Read)]
-    public bool TryGetKey(TValue value, out TKey key) => _backward.TryGetValue(value, out key);
+    public bool TryGetKey(TValue value, [UnscopedRef]  out TKey key) => _backward.TryGetValue(value, out key);
 
     /// <inheritdoc />
     /// <exception cref="T:System.InvalidOperationException">The property is set and the value is already present, bound to a different key.</exception>
@@ -221,25 +235,86 @@ public class BiDictionary<TKey, TValue> : IBiDictionary<TKey, TValue>
     }
 
     /// <inheritdoc />
-    public TKey this[TValue valueKey] {
-        [CollectionAccess(CollectionAccessType.Read)]
-        get => _backward[valueKey];
-
-        [CollectionAccess(CollectionAccessType.ModifyExistingContent)]
-        set {
-            if (_forward.TryGetValue(value, out var oldValue) && !_backward.Comparer.Equals(oldValue, valueKey))
-                throw new InvalidOperationException("The key is already present and bound to a different value.");
-
-            _backward[valueKey] = value;
-            _forward[value] = valueKey;
-        }
-    }
-
-    /// <inheritdoc />
     [CollectionAccess(CollectionAccessType.Read)]
     public ICollection<TKey> Keys => _forward.Keys;
 
     /// <inheritdoc />
     [CollectionAccess(CollectionAccessType.Read)]
     public ICollection<TValue> Values => _forward.Values;
+
+    private sealed class InverseImpl : IBiDictionary<TValue, TKey>
+    {
+        private readonly BiDictionary<TKey, TValue> _owner;
+
+        public InverseImpl(BiDictionary<TKey, TValue> owner) => _owner = owner;
+
+        public IEnumerator<KeyValuePair<TValue, TKey>> GetEnumerator() => _owner._backward.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void Add(KeyValuePair<TValue, TKey> item) => _owner.Add(item.Value, item.Key);
+
+        public void Clear() => _owner.Clear();
+
+        public bool Contains(KeyValuePair<TValue, TKey> item)
+        {
+            if (!_owner._backward.TryGetValue(item.Key, out var key))
+                return false;
+
+            if (ReferenceEquals(key, null))
+                return false;
+
+            return _owner._forward.Comparer.Equals(key, item.Value);
+        }
+
+        public void CopyTo(KeyValuePair<TValue, TKey>[] array, int arrayIndex)
+            => (_owner._backward as ICollection<KeyValuePair<TValue, TKey>>).CopyTo(array, arrayIndex);
+
+        public bool Remove(KeyValuePair<TValue, TKey> item)
+        {
+            var forwardSuccess = (_owner._forward as ICollection<KeyValuePair<TKey, TValue>>).Remove(new KeyValuePair<TKey, TValue>(item.Value, item.Key));
+            var backwardSuccess = (_owner._backward as ICollection<KeyValuePair<TValue, TKey>>).Remove(item);
+
+            Debug.Assert(forwardSuccess == backwardSuccess);
+
+            return forwardSuccess;
+        }
+
+        public int Count => _owner.Count;
+
+        public bool IsReadOnly => _owner.IsReadOnly;
+
+        public void Add(TValue key, TKey value) => _owner.Add(value, key);
+
+        public bool ContainsKey(TValue key) => _owner.ContainsValue(key);
+
+        public bool Remove(TValue key) => _owner.RemoveValue(key);
+
+        public bool TryGetValue(TValue key, [UnscopedRef] out TKey value) => _owner.TryGetKey(key, out value);
+
+        public TKey this[TValue valueKey] {
+            [CollectionAccess(CollectionAccessType.Read)]
+            get => _owner._backward[valueKey];
+
+            [CollectionAccess(CollectionAccessType.ModifyExistingContent)]
+            set {
+                if (_owner._forward.TryGetValue(value, out var oldValue) && !_owner._backward.Comparer.Equals(oldValue, valueKey))
+                    throw new InvalidOperationException("The key is already present and bound to a different value.");
+
+                _owner._backward[valueKey] = value;
+                _owner._forward[value] = valueKey;
+            }
+        }
+
+        public ICollection<TValue> Keys => _owner.Values;
+
+        public ICollection<TKey> Values => _owner.Keys;
+
+        public IBiDictionary<TKey, TValue> Inverse => _owner;
+        public bool ContainsValue(TKey value) => _owner.ContainsKey(value);
+
+        public bool RemoveValue(TKey value) => _owner.Remove(value);
+
+        public bool TryGetKey(TKey value, [UnscopedRef] out TValue key) => _owner.TryGetValue(value, out key);
+    }
 }
