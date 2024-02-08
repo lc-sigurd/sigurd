f023fac99320a6cae62cdccb8c49b2dd6f79e74e
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-08 22:40:04 +0000

    add registry configuration dataclass

---
+++ SigurdLib.Common/Registries/RegistryConfiguration.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Sigurd.Common.Registries;
+
+public record RegistryConfiguration<TValue> where TValue : class
+{
+    private const int DefaultMaxInt = Int32.MaxValue - 1;
+
+    public int MinId { get; set; } = 0;
+    public int MaxId { get; set; } = DefaultMaxInt;
+
+    public bool AllowModifications { get; set; } = false;
+
+    public bool Taggable { get; set; } = false;
+
+    /// <summary>
+    /// Event invoked when contents are added to the registry. This will be invoked when the registry
+    /// is rebuilt on the client side due to a server-side synchronization.
+    /// </summary>
+    public EventHandler<ISigurdRegistry.AddEventArgs<TValue>>? AddCallback;
+
+    /// <summary>
+    /// Event invoked when the registry's contents are cleared. This will be invoked before a registry
+    /// is rebuilt.
+    /// </summary>
+    public EventHandler<ISigurdRegistry.ClearEventArgs<TValue>>? ClearCallback;
+
+    /// <summary>
+    /// Event invoked when a registry instance is initially created.
+    /// </summary>
+    public EventHandler<ISigurdRegistry.CreateEventArgs<TValue>>? CreateCallback;
+
+    /// <summary>
+    /// Event invoked when the registry's contents are validated.
+    /// </summary>
+    public EventHandler<ISigurdRegistry.ValidateEventArgs<TValue>>? ValidateCallback;
+
+    /// <summary>
+    /// Event invoked when the registry has finished processing.
+    /// </summary>
+    public EventHandler<ISigurdRegistry.BakeEventArgs<TValue>>? BakeCallback;
+}
