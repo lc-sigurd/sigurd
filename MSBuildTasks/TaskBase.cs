61e8bb4df5c14970ed19ffb27a180113a6162116
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-16 15:56:43 +0000

    add TaskBase that exposes ProjectInstance

---
+++ MSBuildTasks/TaskBase.cs
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024 The Sigurd Team
+ * The Sigurd Team licenses this file to you under the LGPL-3.0-OR-LATER license.
+ */
+
+using Microsoft.Build.Execution;
+using MSBuildTasks.Extensions;
+
+namespace MSBuildTasks;
+
+public abstract class TaskBase : Microsoft.Build.Utilities.Task
+{
+    private ProjectInstance? _project;
+    protected ProjectInstance Project => _project ??= BuildEngine.GetProjectInstance();
+
+    protected string ProjectName => Project.GetPropertyValue("ProjectName");
+}
