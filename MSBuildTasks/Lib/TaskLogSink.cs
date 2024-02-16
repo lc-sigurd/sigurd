ab9f76d7aee5c6d2d175a9b9cc7f218584d94e4f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-16 17:46:44 +0000

    update copyright declaration

--- MSBuildTasks/Lib/TaskLogSink.cs
+++ MSBuildTasks/Lib/TaskLogSink.cs
@@ -1,7 +1,15 @@
 /*
+ * The contents of this file have been sourced from various projects, in addition to contributions by the Sigurd Team.
+ *
+ * https://github.com/serilog-contrib/serilog-sinks-msbuild/blob/08bfd97b4ac0523a2b19407ba2325e75b22d6d84/Serilog.Sinks.MSBuild/MSBuildSink.cs
+ * Copyright 2019 Theodore Tsirpanis
+ * Theodore Tsirpanis licenses the contents of the referenced file to the Sigurd Team under the Apache-2.0 license.
+ *
  * https://github.com/EvaisaDev/UnityNetcodePatcher/blob/c64eb86e74e85e1badc442adc0bf270bab0df6b6/NetcodePatcher.MSBuild/TaskLogSink.cs
- * UnityNetcodePatcher Copyright (c) 2023 EvaisaDev.
- * EvaisaDev licenses this file to the Sigurd Team under the MIT license.
+ * UnityNetcodePatcher Copyright (c) 2023 EvaisaDev
+ * EvaisaDev licenses the contents of the referenced file to the Sigurd Team under the MIT license.
+ *
+ * Copyright (c) 2024 The Sigurd Team
  * The Sigurd Team licenses this file to you under the LGPL-3.0-OR-LATER license.
  */
 
