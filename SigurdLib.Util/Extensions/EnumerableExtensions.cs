c9f2f24f2bf2885720e845518e9c3f23ac86ca39
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-19 20:51:19 +0000

    add Enumerable.Tap

--- SigurdLib.Util/Extensions/EnumerableExtensions.cs
+++ SigurdLib.Util/Extensions/EnumerableExtensions.cs
@@ -44,4 +44,24 @@ public static class EnumerableExtensions
         => enumerable
             .Where(maybe => maybe.IsSome)
             .SelectMany(definite => definite.ValueUnsafe!);
+
+    /// <summary>Execute code for every element in a collection and pass-through the original value.</summary>
+    /// <typeparam name="TSource">The inner type of the collection</typeparam>
+    /// <param name="source">The collection</param>
+    /// <param name="action">The action to execute</param>
+    /// <returns>The collection, unchanged</returns>
+    public static IEnumerable<TSource> Tap<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+    {
+        if (source is null) throw new ArgumentNullException(nameof(source));
+        if (action is null) throw new ArgumentNullException(nameof(action));
+        return TapIterator(source, action);
+    }
+
+    private static IEnumerable<TSource> TapIterator<TSource>(IEnumerable<TSource> source, Action<TSource> action)
+    {
+        foreach (TSource element in source) {
+            action(element);
+            yield return element;
+        }
+    }
 }
