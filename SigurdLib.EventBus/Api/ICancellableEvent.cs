ead830255d0aa61682af1cae86fb13fa8a96a430
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-15 13:01:46 +0000

    add `ICancellableEvent` interface

---
+++ SigurdLib.EventBus/Api/ICancellableEvent.cs
@@ -0,0 +1,8 @@
+namespace Sigurd.EventBus.Api;
+
+public interface ICancellableEvent
+{
+    void SetCanceled(bool canceled) => ((Event)this)._isCancelled = canceled;
+
+    bool IsCanceled => ((Event)this)._isCancelled;
+}
