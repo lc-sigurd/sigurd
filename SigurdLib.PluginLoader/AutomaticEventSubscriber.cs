8f817fc94fe7c7169f8404be0307f3ba209f9644
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-15 13:01:38 +0000

    XMLdoc for `AutomaticEventSubscriber` - not implemented yet

--- SigurdLib.PluginLoader/AutomaticEventSubscriber.cs
+++ SigurdLib.PluginLoader/AutomaticEventSubscriber.cs
@@ -1,10 +1,13 @@
+using static Sigurd.PluginLoader.SigurdPlugin.EventBusSubscriberAttribute;
+
 namespace Sigurd.PluginLoader;
 
 /// <summary>
-/// Automatic <see cref="EventBus"/>
+/// Automatic <see cref="EventBus"/> subscriber. Reads <see cref="SigurdPlugin.EventBusSubscriberAttribute"/>
+/// annotations and passes the annotated types to the <see cref="Bus"/> defined by the annotation, which
+/// defaults to <c>SigurdLib.EventBus</c>.
 /// </summary>
 public class AutomaticEventSubscriber
 {
-
 
 }
