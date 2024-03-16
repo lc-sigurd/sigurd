86499bf9fb4757ca94a4309d12805aade6bfa829
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-16 01:46:28 +0000

    add `Assembly` to `PluginContainer`

--- SigurdLib.PluginLoader/PluginContainer.cs
+++ SigurdLib.PluginLoader/PluginContainer.cs
@@ -3,6 +3,7 @@
  * The Sigurd Team licenses this file to you under the LGPL-3.0-OR-LATER license.
  */
 
+using System.Reflection;
 using BepInEx;
 
 namespace Sigurd.PluginLoader;
@@ -11,13 +12,16 @@ public class PluginContainer
 {
     public PluginInfo Info { get; }
 
+    public Assembly Assembly { get; }
+
     public string Guid => Info.Metadata.GUID;
 
     public string Namespace => Guid;
 
-    public PluginContainer(PluginInfo info)
+    public PluginContainer(PluginInfo info, Assembly assembly)
     {
         Info = info;
+        Assembly = assembly;
     }
 
     /// <inheritdoc />
